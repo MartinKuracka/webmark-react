72f4f290d80ca495bb2ef263637beb618ec7caf5
martin kuracka <m.kuracka@gmail.com>
2021-04-16 14:03:20 +0200

    fixed webmark headr image size

--- src/containers/home_section/home.js
+++ src/containers/home_section/home.js
@@ -8,8 +8,8 @@ const HeadSection = () => {
         <div className='flex w-full h-screen flex-col bg-headerImg items-center' id='home'>
             <div className='w-11/12'>
                 <h1 className='text-3xl font-light  mt-24'><span className='text-primary font-extrabold'>W</span>elcome To The</h1>
-                <h1 className='text-6xl'><span className='text-primary font-extrabold'>W</span>ebMark</h1>
-                <img className='-mt-3' src={Webmark} alt={Webmark}/>
+                <h1 className='text-5xl'><span className='text-primary font-extrabold'>W</span>ebMark</h1>
+                <img className='-mt-3 w-96' src={Webmark} alt={Webmark}/>
                 <img className='mt-5' src={Hero} alt='hero' />
             </div>
         </div>
